e7facbed94080d7c6262af583b4774a4bf9cfa51
stillyearzero <stillyearzero@gmail.com>
2025-07-02 11:24:49 +0200

    General update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navbar = document.querySelector(".navbar");
-const body = document.querySelector("main");
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -9,7 +8,6 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     navbar.classList.toggle("active");
-    body.classList.toggle("active");
 }
 
 const navLink = document.querySelectorAll(".nav-link");
@@ -20,5 +18,4 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
     navbar.classList.remove("active");
-    body.classList.remove("active");
 }
